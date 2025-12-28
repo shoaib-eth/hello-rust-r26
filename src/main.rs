2573b475ddc202c51b0c5a38edaefde3944c6193
Shoaib Khan <shoaibkhan@shoaibs-MacBook-Air.local>
2025-12-28 16:19:46 +0530

    Avoiding Ownership By Using

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::usize;
 
 fn main() {
-    let s1 = String::from("Hello");  // here s1 is the owner
-    let (s2, len) = calculate_string(s1); // Transfer ownership from s1 to len
+    let s1 = String::from("Hello");  
+    let len = calculate_string(s1.clone()); // We've copied the s1 value and send to the function, here ownership does'nt exists
 
-    println!("The length of {} is {}", s2, len);
+    println!("The length of {} is {}", s1, len);
 }
 
-fn calculate_string(s: String) -> (String, usize) {  // s will be the new owner
+fn calculate_string(s: String) -> usize {  
     let length = s.len();
-    return (s, length);
+    return length;
 }
+
+// `clone()` method is a expensive approach for memory, because it copies the value and consume extra memory slot with the same value.
