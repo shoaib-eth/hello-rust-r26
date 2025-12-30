4361b9625b435e227096f3c0d2307955273775e6
Shoaib Khan <shoaibkhan@shoaibs-MacBook-Air.local>
2025-12-30 16:19:06 +0530

    Reference Rules - 2

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,15 @@
 fn main() {
-    let s1: String = String::from("Hello");
-    let r1 = &s1;
-    let r2 = &s1;
+    let mut s1: String = String::from("Hello");
+    let w1 = &mut s1;
+    w1.push_str(" World");
+    println!("w1 = {}", w1);
 
-    println!("r1 = {}, r2 = {}", r1, r2);
+    let w2 = &mut s1;
+    w2.push_str(" Code");
+    println!("w2 = {}", w2);
+
+    // println!("w1 = {}, w2 = {}", w1, w2);  // This line will give an error, because we write all borrowed value at once
 }
 
-/*
-Notes 📝
-
-    &s1 creates an immutable reference.
-    You can have multiple immutable references at the same time.
-    Immutable references allow read-only access.
-    No ownership is moved from s1.
-
-    ```
-    let r1 = &s1;
-    let r2 = &s1;
-    ```
-    ✔️ This is valid because both are immutable borrows.
-
-📤 Output works because: r1 and r2 only read the value and No modification happens
-
-🔑 One-Line Rule
-
-👉 Many readers allowed, no writers 📖📖❌✍️
-*/
+// Here write performance can be easily executed
+// but if we write only once, it would give an error 
