c40e3b8ad90f8ac8d459eb40e3831e32d0755ab1
Shoaib Khan <shoaibkhan@shoaibs-MacBook-Air.local>
2025-12-30 16:26:56 +0530

    Reference Rules - 3

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
     let mut s1: String = String::from("Hello");
+    let r2 = &s1;
+    println!("r1 = {}", r2);
+
     let w1 = &mut s1;
-    w1.push_str(" World");
+    w1.push_str("World");
     println!("w1 = {}", w1);
-
-    let w2 = &mut s1;
-    w2.push_str(" Code");
-    println!("w2 = {}", w2);
-
-    // println!("w1 = {}, w2 = {}", w1, w2);  // This line will give an error, because we write all borrowed value at once
 }
 
-// Here write performance can be easily executed
-// but if we write only once, it would give an error 
+// This code will successfully executed because it is not implement multiple read and write operations
+
+// Summary 🗒️
+
+// Multiple read and write operations is not allowed in borrow
