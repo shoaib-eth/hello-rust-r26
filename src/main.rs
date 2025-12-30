150287375d3b5f48dbb87fac7a0b63ba06b2db8b
Shoaib Khan <shoaibkhan@shoaibs-MacBook-Air.local>
2025-12-30 14:27:20 +0530

    Borrowing Without Mutability

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-use std::usize;
-
 fn main() {
     let s1 = String::from("Hello");  
-    let len = calculate_string(s1.clone()); // We've copied the s1 value and send to the function, here ownership does'nt exists
+    let len = calculate_string(&s1); // Here we done borrow option by applying `&` with `s1`, which means we borrow s1's refrence for few time
 
     println!("The length of {} is {}", s1, len);
 }
 
-fn calculate_string(s: String) -> usize {  
+fn calculate_string(s: &String) -> usize {  // `&` also applied here in data type
     let length = s.len();
     return length;
 }
 
-// `clone()` method is a expensive approach for memory, because it copies the value and consume extra memory slot with the same value.
+
+// Note 📝
