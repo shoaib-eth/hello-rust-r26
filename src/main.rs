3766bc66f1b22847b19853fa2bd781370dd6efcc
Shoaib Khan <shoaibkhan@shoaibs-MacBook-Air.local>
2026-01-01 15:36:23 +0530

    Ownership in Vector

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,17 @@
-// Vector Dynamic Array
-
 fn main() {
-    let mut v: Vec<i32> = Vec::new(); // Vector Declaration
-    let mut v1 = Vec::<i32>::new(); // Another Way For Vector Declaration
-    let mut v2 = vec![1, 2, 3, 4, 5]; // Initialized Vector like Fixed Type Array, we an change this in future by `push()` 
-
-    v.push(10);
-    v.push(11);
-    v.push(12);
-
-    v1.push(20);
-    v1.push(21);
-    v1.push(22);
-
-    println!("Vector v = {:?}", v);
-    println!("Vector v1 = {:?}", v1);
-    println!("Vector v2 = {:?}", v2);
-
-    // Add new value to v2 (Initialized Vector)
-    v2.push(6);
-    println!("Vector v2 = {:?}", v2);
-
-    // Delete a value from v2 
-    v2.pop();
-    println!("Vector v2 = {:?}", v2);
-
-    println!("Length of v2 = {}", v2.len());
+    let vrr: Vec<&str> = vec!["Hello", "World", "Rustians"]; // Here, `vrr` is the owner of all the array elements
+    write_vrr(vrr); // ownership transfered
+    
+    // println!("vrr = {:?}", vrr);  // Compile time error
 }
 
+fn write_vrr(vrr2: Vec<&str>) { // `vrr2` is the current owner of `vrr`
+    println!("vrr2 = {:?}", vrr2);
+}
 
-// Output:
+// Important Notes 📝
 
-/* 
-Vector v = [10, 11, 12]
-Vector v1 = [20, 21, 22]
-Vector v2 = [1, 2, 3, 4, 5]
-Vector v2 = [1, 2, 3, 4, 5, 6]
-Vector v2 = [1, 2, 3, 4, 5]
-Length of v2 = 5
-*/
+// 1. Ownership is exist in vector
+
+/* Onwnership Tranfer */
+// vrr  - >  Ownership Transfered through write_vrr()  ->  vrr2
