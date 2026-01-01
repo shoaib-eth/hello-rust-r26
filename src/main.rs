64056b1f78f900a263bcfa3092a6d5972545f301
Shoaib Khan <shoaibkhan@shoaibs-MacBook-Air.local>
2026-01-01 14:51:09 +0530

    Inexpensive Array Reading

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let arr: [&str; 3] = ["Hello", "World", "Coders"];
-    read_arr(arr); // Array directly passed
+    read_arr(&arr); // Array directly passed
     println!("arr = {:?}", arr);
 }
 
-// This could be expensive, because it copies the array then read
-fn read_arr(arr1: [&str; 3]) {
+// It is inexpensive way of reading array, because we passed the array reference only, not copy the entire array
+fn read_arr(arr1: &[&str; 3]) {
     println!("arr2 = {:?}", arr1);
 }
 
