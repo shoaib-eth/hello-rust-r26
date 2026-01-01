09a2ecd4f586c004290dfeae3dec92467a1b01b4
Shoaib Khan <shoaibkhan@shoaibs-MacBook-Air.local>
2026-01-01 14:44:46 +0530

    Reference Passing Into Array

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let arr: [&str; 3] = ["Hello", "World", "Coders"];
-    write_arr(arr); // Array directly passed
+    let mut arr: [&str; 3] = ["Hello", "World", "Coders"];
+    write_arr(&mut arr); // Array directly passed
     println!("arr = {:?}", arr);
 }
 
-fn write_arr(mut arr1: [&str; 3]) { // arr1 new copy of arr
+fn write_arr(arr1: &mut [&str; 3]) { // arr1 new copy of arr
     arr1[0] = "Hey 👋";
     println!("arr1 = {:?}", arr1);
 }
@@ -12,7 +12,6 @@ fn write_arr(mut arr1: [&str; 3]) { // arr1 new copy of arr
 // Output:
 
 // arr1 = ["Hey 👋", "World", "Coders"]
-// arr = ["Hello", "World", "Coders"]
+// arr = ["Hey 👋", "World", "Coders"]
 
-// There is no ownership concept because array is fixed type
-// But this could be very expensive for memory because it copies entire array, and array could be very long.
+// The the result will be same because it points only one array and some changes in it
