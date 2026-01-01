91b058e1acf985c11967f900f72280645d7d3a0c
Shoaib Khan <shoaibkhan@shoaibs-MacBook-Air.local>
2026-01-01 16:07:02 +0530

    for loop

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 fn main() {
-    let number = 12;
+    let arr = [1, 2, 3, 4, 5];
 
-    if number % 3 == 0 && number % 4 == 0 {
-        println!("The number is divisible by both 3 and 4.");
-    } else if number % 3 == 0 {
-        println!("The number is divisible by 3 but not by 4.");
-    } else if number % 4 == 0 {
-        println!("The number is divisible by 4 but not by 3.");
-    } else {
-        println!("The number is neither divisible by 3 nor by 4.");
+    for element in &arr {
+        println!("{}", element);
     }
 }
+
+// `for loop` has different syntax in rust, look at them in `main()` function
