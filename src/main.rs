63961942c8348cb46bddd491ddcd3e6f95ab3942
Shoaib Khan <shoaibkhan@shoaibs-MacBook-Air.local>
2026-01-01 16:40:11 +0530

    I/O

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
+use std::io;
+
 fn main() {
-    let num = 11;
+    let mut input = String::new();
+    println!("please Enter Your Input: ");
+    io::stdin()
+       .read_line(&mut input)
+       .expect("Failed To Read Input");
 
-    fn is_even(num: u8) -> bool{
-        // we can do this in most easiest way like this only in one line
-        return num % 2 == 0;
-        // if num % 2 == 0 {
-        //     return true;
-        // } 
-        // return false;
-    }
-
-    // `is_even()` function calling in `match` function
-    match num {
-        x if is_even(x) => println!("Even"),
-        x if !is_even(x) => println!("Odd"),
-        _=>println!("Number Is Not Recognizible")
-    }
+    println!("User Input: {}", input);
 }
+
+
+// This is how we take input from user
