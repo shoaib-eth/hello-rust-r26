a2c9839ae0281d137c726786c67294f0c638222b
shoaib-eth <shoaib.developer69@gmail.com>
2026-01-05 17:44:00 +0530

    impl, Associative Function & Methods

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,54 @@ struct Rectangle {
     breadth: u16,
 }
 
-fn area_rec(rec: &Rectangle) -> u16 {
-    // value is borrowed
-    return rec.length * rec.breadth;
-}
+impl Rectangle {
+    // Associative Functions
+    fn new(new_length: u16, new_breadth: u16) -> Self {
+        Self {
+            length: new_length,
+            breadth: new_breadth,
+        }
+    }
 
-fn change_len(rec: &mut Rectangle) {
-    rec.length = 25;
+    // Methods
+    fn area(&self) -> u16 {
+        return self.length * self.breadth;
+    }
+
+    fn change_breadth(&mut self) {
+        self.breadth = 5;
+    }
 }
 
 fn main() {
-    let mut rec_one: Rectangle = Rectangle {
-        length: 10,
-        breadth: 5,
-    };
+    let rec_one: Rectangle = Rectangle::new(10, 5);
+    let mut rec_two: Rectangle = Rectangle::new(20, 15);
 
-    let mut rec_two: Rectangle = Rectangle {
-        length: 20,
-        breadth: 15,
-    };
+    // Now we're calculating area of rectangle
+    let result_one = rec_one.area(); // Instead of passing ownership, it borrows the value
+    println!("rec_one: {:?}", rec_one);
+    println!("Result One: {}", result_one);
 
-    change_len(&mut rec_one);
-    change_len(&mut rec_two);
+    let result_two = rec_two.area(); // Instead of passing ownership, it borrows the value
+    println!("rec_two: {:?}", rec_two);
+    println!("Result Two: {}", result_two);
 
-    println!("rec_one = {:?}", rec_one);
-    // let result_one: u16 = area_rec(&rec_one); // value is borrowed
-    // println!("Result One: {}", result_one);
-
-    println!("rec_two = {:?}", rec_two);
-    // let result_two: u16 = area_rec(&rec_two); // value is borrowed
-    // println!("Result Two: {}", result_two);
+    // If we want to change something in `rec_two`, we have to make it `mut` and in `change_breadth()` make it `&mut`
+    rec_two.change_breadth();
+    println!("rec_two new breadth: {:?}", rec_two);
 }
+
+// Notes 📝
+
+// To know which is the `Associative Function` and which os the `Methods`, there are some practices
+
+// 1. In Associative Function we write capital `Self` param
+// 2. In Methods we write small `self` param
+
+// It is very efficient way of coding in rust because we have to specify our logic in `impl` and we can run that by calling, and we dont need to update our whole code we have to update only `impl` block code and everything will run smoothly.
+
+// For More Details Read These Notes
+
+// This Topic Notes Starts From 14th PPT
+
+// https://docs.google.com/presentation/d/1t7IMRMdkR3an0-YofAF9M0r9od_0kcfl03UCjdE5njQ/edit?slide=id.g318101e5d17_0_61#slide=id.g318101e5d17_0_61
