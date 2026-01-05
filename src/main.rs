09cda0a6176452aaeac91652d6b6226b8e0c8df6
shoaib-eth <shoaib.developer69@gmail.com>
2026-01-05 16:43:11 +0530

    Struct: mutable reference

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,29 @@ fn area_rec(rec: &Rectangle) -> u16 {
     return rec.length * rec.breadth;
 }
 
+fn change_len(rec: &mut Rectangle) {
+    rec.length = 25;
+}
+
 fn main() {
-    let rec_one: Rectangle = Rectangle {
+    let mut rec_one: Rectangle = Rectangle {
         length: 10,
         breadth: 5,
     };
 
-    let rec_two: Rectangle = Rectangle {
+    let mut rec_two: Rectangle = Rectangle {
         length: 20,
         breadth: 15,
     };
 
+    change_len(&mut rec_one);
+    change_len(&mut rec_two);
+
     println!("rec_one = {:?}", rec_one);
-    let result_one: u16 = area_rec(&rec_one); // value is borrowed
-    println!("Result One: {}", result_one);
+    // let result_one: u16 = area_rec(&rec_one); // value is borrowed
+    // println!("Result One: {}", result_one);
 
     println!("rec_two = {:?}", rec_two);
-    let result_two: u16 = area_rec(&rec_two); // value is borrowed
-    println!("Result Two: {}", result_two);
+    // let result_two: u16 = area_rec(&rec_two); // value is borrowed
+    // println!("Result Two: {}", result_two);
 }
