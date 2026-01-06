2d49d56d12bc4490006d5978728e8a994936f59c
shoaib-eth <shoaib.developer69@gmail.com>
2026-01-06 14:58:44 +0530

    Generic Types

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-fn divide(x: i32, y: i32) -> Result<i32, String> {
-    if y == 0 {
-        return Err("Y is Zero".to_owned());
-    } else {
-        return Ok(x / y);
-    }
+use std::fmt::Display;
+
+// This syntax will help to print all data type values
+// This is Generic Type
+// Here we use <T: Displat>, T, this will help to read all the data types and printing it all.
+fn print_data<T: Display>(data: T) -> T {
+    // println!("Data: {}", data);
+    data
 }
 
 fn main() {
-    let result: Result<i32, String> = divide(10, 5);
-    println!("Result: {:?}", result);
+    let x = 5;
+    let y = true;
+    let z = "Hello".to_owned();
+
+    println!("Data: {}", print_data(x));
+    println!("Data: {}", print_data(y));
+    println!("Data: {}", print_data(z));
 }
