6200b0cecab71c6f6e98970a6fc65b50ae0769b0
shoaib-eth <shoaib.developer69@gmail.com>
2026-01-07 12:33:00 +0530

    Assignment - 1

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,62 @@
-#[derive(Debug)]
-
-struct Circle {
-    radius: f64,
+trait Course {
+    fn get_overview(&self) -> String;
 }
 
-const PI: f64 = 3.14;
-
-trait Shape {
-    fn area_of_circle(&self) -> f64;
+struct Workshop {
+    title: String,
+    instructore: String,
+    duration: String,
 }
 
-impl Shape for Circle {
-    fn area_of_circle(&self) -> f64 {
-        return PI * self.radius * self.radius;
+struct Seminar {
+    title: String,
+    speaker: String,
+    location: String,
+}
+
+// Here we used `format!()`, it returns the string
+impl Course for Workshop {
+    fn get_overview(&self) -> String {
+        format!(
+            "Workshop Title: {}, Workshop Instructor: {}, Workshop Duration: {}",
+            self.title, self.instructore, self.duration
+        )
     }
 }
 
+impl Course for Seminar {
+    fn get_overview(&self) -> String {
+        format!(
+            "Seminar Title: {}, Seminar Speaker: {}, Seminar Location: {}",
+            self.title, self.speaker, self.location
+        )
+    }
+}
+
+fn print_course_overview<T: Course>(param: T) {
+    println!("{}", param.get_overview());
+}
+
 fn main() {
-    let circle: Circle = Circle { radius: 5.0 };
-    println!("Area of circle: {:?}", circle.area_of_circle());
+    let workshop: Workshop = Workshop {
+        title: "Blockchain".to_owned(),
+        instructore: "Alice".to_owned(),
+        duration: "4 Hours".to_owned(),
+    };
+
+    let seminar: Seminar = Seminar {
+        title: "Bitcoin: The Digital Gold".to_owned(),
+        speaker: "Shoaib".to_owned(),
+        location: "Delhi, India".to_owned(),
+    };
+
+    // call the functions
+    // workshop.get_overview();
+    // seminar.get_overview();
+
+    // println!("Workshop Details -> {}", workshop.get_overview());
+    // println!("Seminar Details -> {}", seminar.get_overview());
+
+    print_course_overview(workshop);
+    print_course_overview(seminar);
 }
