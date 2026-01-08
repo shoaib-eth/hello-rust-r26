d998fb1b01ecd6a4ac24acdfa9dafa4a56c56054
shoaib-eth <shoaib.developer69@gmail.com>
2026-01-08 15:27:01 +0530

    Iterators:

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
     let a = [1, 2, 3];
 
-    let mut iter = a.into_iter(); // `into_iter() iterates directly over the elements and get the elements ownership dirctly to the iterator`
+    let mut iter = a.iter(); // `iter()` does not owns the wonership of all elements, it get the reference of those elements, it does not returns the elements value while it returns the reference of those elements
 
     // A call to next() returns the next value.....
-    assert_eq!(Some(1), iter.next());
-    assert_eq!(Some(2), iter.next());
-    assert_eq!(Some(3), iter.next());
+    assert_eq!(Some(&1), iter.next());
+    assert_eq!(Some(&2), iter.next());
+    assert_eq!(Some(&3), iter.next());
 
     // ... and then None once it's over
     assert_eq!(None, iter.next());
@@ -15,3 +15,10 @@ fn main() {
     assert_eq!(None, iter.next());
     assert_eq!(None, iter.next());
 }
+
+// Note 📝
+
+// Difference Between `into_iter()` and `iter()`
+
+// `into_iter()` -> It get the ownership of elements directly and returns the elements value
+// `iter()` -> It get the reference of those elemens and returns the elements reference
