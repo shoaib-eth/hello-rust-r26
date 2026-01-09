2415cec040829347abbf9395a236df9303eb7592
shoaib-eth <shoaib.developer69@gmail.com>
2026-01-09 16:59:41 +0530

    Assignment - 2: Iterators

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,32 @@
-fn main() {
-    let arr = [1, 2, 3];
-
-    for item in &arr {
-        // Here `arr` is `iter()` type
-        println!("{}", item);
-    }
-    println!("{:?}", arr);
-
-    for item in arr {
-        // Here `arr` is `into_iter()` type
-        println!("{}", item);
-    }
-    println!("{:?}", arr);
+struct Counter {
+    count: u32,
 }
 
-// Notes 📝
+impl Counter {
+    fn new() -> Self {
+        Self { count: 0 }
+    }
+}
 
-// Here we printed the both `println!()` statements.
+// Here we have flexibility for future development in this code, because we have to change data type in only `Item` type and change struct name in `impl Iterator for <new_struct_name>`. we have to decide and write at once and dont need to touch other lines of code.
+impl Iterator for Counter {
+    type Item = u32; // `type` keyword is for creating alias for the data types, that means where we write `Item` in our code which means we write `u32`. this is required in `Iterator` case 
 
-// because array is a fixed type and fixed types elements always stores in stack and give us a copy trait
-// that's why our seconds `println!()` statements runs successfully and doesn't gave an error
-// here ownership works but doesn't impact the code running
+    fn next(&mut self) -> Option<Self::Item> {
+        // Here `Self` denotes the `Counter` struct. but we cannot write `Counter` directly here because we dealing with the traits, and this is required to sepcify like this.
+        if self.count < 5 {
+            self.count = self.count + 1;
+            return Some(self.count);
+        } else {
+            return None;
+        }
+    }
+}
 
-// if try this in `vector` case, so our second `println!()` statement will give an error, why? because vector is dynamic type and its elements stored in heap memory, so here ownership works stricktly.
+fn main() {
+    let count = Counter::new();
 
-// Fixed Type -> Give Copy Trait
-// Dynamic Type -> Don't Give Copy Trait, Ownership Works Stricktly.
+    for val in count {
+        println!("Count: {}", val)
+    }
+}
