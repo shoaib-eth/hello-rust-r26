67f53146a4dc3a7a5b699e07cd73208d29654b64
shoaib-eth <shoaib.developer69@gmail.com>
2026-01-09 17:08:50 +0530

    Mini Assignment For Type Keyword Understanding

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,13 @@
-struct Counter {
-    count: u32,
-}
+type Kilometer = i32;
 
-impl Counter {
-    fn new() -> Self {
-        Self { count: 0 }
-    }
-}
-
-// Here we have flexibility for future development in this code, because we have to change data type in only `Item` type and change struct name in `impl Iterator for <new_struct_name>`. we have to decide and write at once and dont need to touch other lines of code.
-impl Iterator for Counter {
-    type Item = u32; // `type` keyword is for creating alias for the data types, that means where we write `Item` in our code which means we write `u32`. this is required in `Iterator` case 
-
-    fn next(&mut self) -> Option<Self::Item> {
-        // Here `Self` denotes the `Counter` struct. but we cannot write `Counter` directly here because we dealing with the traits, and this is required to sepcify like this.
-        if self.count < 5 {
-            self.count = self.count + 1;
-            return Some(self.count);
-        } else {
-            return None;
-        }
-    }
+fn add_distance(x: Kilometer, y: Kilometer) -> Kilometer {
+    return x + y;
 }
 
 fn main() {
-    let count = Counter::new();
-
-    for val in count {
-        println!("Count: {}", val)
-    }
+    let a: Kilometer = 5;
+    let b: Kilometer = 10;
+    println!("Total: {}", add_distance(a, b));
 }
+
+// Here we defined Kilometer type
