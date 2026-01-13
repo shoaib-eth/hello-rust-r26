59d0cf96daa91d7b1184f6af6a605282cdad23d3
shoaib-eth <shoaib.developer69@gmail.com>
2026-01-13 17:11:22 +0530

    Closures

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,10 @@
-use clap::Parser;
-use maud::{DOCTYPE, Markup, html};
-use pulldown_cmark::{Options, Parser as MarkdownParser, html};
-use std::{fs, path::PathBuf};
-#[derive(Parser, Debug)]
-
-struct Args {
-    /// Input Markdown File Path
-    #[arg(long, short)]
-    input: PathBuf,
-
-    /// Output HTML File Path
-    #[arg(long, short)]
-    output: Option<PathBuf>,
-}
-
-fn render_html_page(content: &str) -> Markup {
-    html! {
-        (DOCTYPE)
-        html {
-            head {
-                meta charset = "utf-8";
-                title { "Markdown To HTML Output" }
-            }
-            body {
-                (maud::PreEscaped(content.to_string()))
-            }
-        }
-    }
-}
-
 fn main() {
-    let args = Args::parse();
-    let markdown_input = fs::read_to_string(&args.input).expect("Failed To Read To String");
+    let x = String::from("Hello");
+    let consume_and_return_x = || &x;
+    println!("{}", x);
+    let y = consume_and_return_x();
+    println!("{}", y);
 
-    let mut options = Options::empty();
-    options.insert(Options::ENABLE_STRIKETHROUGH);
-
-    let parser = MarkdownParser::new_ext(&markdown_input, options);
-
-    let mut html_output = String::new();
-    html::push_html(&mut html_output, parser);
-
-    let full_html_output = render_html_page(&html_output).into_string();
-
-    match &args.output {
-        Some(path) => fs::write(path, full_html_output).expect("Failed To Write!"),
-        None => println!("Path Not Provided"),
-    }
+    let z = y;
+    println!("{}", z);
 }
