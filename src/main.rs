87e319022181aaebf45f5d7e4d70def3fb43a7d5
shoaib-eth <shoaib.developer69@gmail.com>
2026-01-14 14:24:01 +0530

    Packages and crates

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
 fn main() {
-    let num = vec![1, 2, 3, 4, 5];
-
-    let result = num
-        .iter()
-        .filter(|x| *x % 2 != 0) // Keeps only odd numbers - [1, 3, 5]
-        .map(|x| *x + 1) // Increment each odd number - [2, 4, 6]
-        .find(|x| *x == 6); // find first item equal to 6 -> Some(6)
-
-    println!("{:?}", result.unwrap()); // print 6
+    // Read Notes on `packages_and_crates.md`
 }
