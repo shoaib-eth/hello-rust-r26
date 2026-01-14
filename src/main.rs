f555523bcbad10b556ef25df290b8da4ef35bcb6
shoaib-eth <shoaib.developer69@gmail.com>
2026-01-14 15:07:09 +0530

    Smart Pointers

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-use std::collections::HashMap;
-
 fn main() {
-    let mut students: HashMap<String, u32> = HashMap::new();
-
-    students.insert("Ravi".to_owned(), 100);
-    students.insert("Raju".to_owned(), 10);
-    students.insert("Lalu".to_owned(), 1);
-
-    for (student, marks) in students.iter() {
-        println!("Student name: {:?} marks={}", student, marks);
-    }
-
-    students.insert("Raju".to_owned(), 200);
-
-    match students.get("Raju") {
-        Some(marks) => println!("Found: {}", marks),
-        None => println!("Not Found"),
-    }
+    // Read Notes on `smart_pointers.md`
 }
