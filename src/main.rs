a29f820e85a9e991502f803c2484a2b99bd4568c
shoaib-eth <shoaib.developer69@gmail.com>
2026-01-15 15:22:26 +0530

    Rust Lifetimes – Lifetime Parameters, Ambiguity & Struct Errors

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,4 @@
-// `print_refs` takes two references to `i32` which have different
-// lifetimes `'a` and `'b`. These two lifetimes must both be at
-// least as long as the function `print_refs`.
-fn print_refs<'a, 'b>(x: &'a i32, y: &'b i32) {
-    println!("x is {} and y is {}", x, y);
-}
-
 fn main() {
-    // Create variables to be borrowed below.
-    let (four, nine) = (4, 9);
-
-    // Borrows (`&`) of both variables are passed into the function.
-    print_refs(&four, &nine);
-
-    // Any input which is borrowed must outlive the borrower.
-    // In other words, the lifetime of `four` and `nine` must
-    // be longer than that of `print_refs`.
+    
 }
+
