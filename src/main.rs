9affe48d8e57f4ef996b3415f5754faad471691b
shoaib-eth <shoaib.developer69@gmail.com>
2026-01-15 12:56:16 +0530

    Rust Lifetimes – Explicit Lifetime Annotations

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-// This code will give an compile time error, read its `notes.md` file
-
-fn main() {
-    let x = 10;
-    let y = get_value();
-
-    println!("x + y = {}", x + y);
+// `print_refs` takes two references to `i32` which have different
+// lifetimes `'a` and `'b`. These two lifetimes must both be at
+// least as long as the function `print_refs`.
+fn print_refs<'a, 'b>(x: &'a i32, y: &'b i32) {
+    println!("x is {} and y is {}", x, y);
 }
 
-fn get_value() -> &i32 {
-    let y = 5;
-    &y
+fn main() {
+    // Create variables to be borrowed below.
+    let (four, nine) = (4, 9);
+
+    // Borrows (`&`) of both variables are passed into the function.
+    print_refs(&four, &nine);
+
+    // Any input which is borrowed must outlive the borrower.
+    // In other words, the lifetime of `four` and `nine` must
+    // be longer than that of `print_refs`.
 }
